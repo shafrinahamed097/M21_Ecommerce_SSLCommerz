12166467b6f2d169ffc4dee2fd4c563e0b4cebbb
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-01-03 21:09:27 +0600

    declare read profile function

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,16 +10,21 @@ use App\Models\CustomerProfile;
 class ProfileController extends Controller
 {
  
-    public function CreateProfile(Request $request){
-        $user_id = $request->header('id');
-        $request->merge(['user_id'=>$user_id]);
-        $data = CustomerProfile::updateOrCreate([
-            'user_id'=>$user_id
-        ],
-    $request->input()
-    );
-
+   public function CreateProfile(Request $request){
+    $user_id = $request->header('id');
+    $request->merge(['user_id'=>$user_id]);
+    $data = CustomerProfile::updateOrCreate(['user_id'=>$user_id], $request->input);
     return ResponseHelper::Out('success', $data,200);
-    }
+   }
+
+  public function ReadProfile(Request $request){
+    $user_id = $request->header('id');
+    $data = CustomerProfile::where('user_id', $user_id)->with('user')->first();
+    return ResponseHelper::Out('success', $data,200);
+
+  }
+
+   
+
     
 }
