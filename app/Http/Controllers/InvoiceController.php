ce79575968d475116560d3a2f14b6beb56619713
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-01-12 12:10:26 +0600

    Create Invoice

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -1,38 +1,57 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Models\ProductCart;
+use App\Models\Invoice;
 
-use App\Models\CustomerProfile;
+use App\Models\ProductCart;
 use Illuminate\Http\Request;
+use App\Models\CustomerProfile;
 use Illuminate\Support\Facades\DB;
 
 
 class InvoiceController extends Controller
 {
 
-   function InvoiceCreate(Request $request){
-    DB::beginTransaction();
 
-    $user_id = $request->header('id');
-    $user_email = $request->header('email');
+    function InvoiceCreate(Request $request){
+        DB::beginTransaction();
 
-    $Profile = CustomerProfile::where('user_id', '=', $user_id)->first();
-    $cus_details = "Name: $Profile->cus_name, Address: $Profile->use_add, City: $Profile->cus_city, Phone: $Profile->cus_phone";
-    $ship_details = "Name: $Profile->ship_name, Address: $Profile=>ship_add, City-> $Profile->ship_city, Phone: $Profile->phone";
+        $user_id = $request->header('id');
+        $user_email = $request->header('email');
 
-    // Payable Calculation
-    $total = 0;
-    $cartList = ProductCart::where('user_id', '=', $user_id)->get();
-    forEach($cartList as $cartItem){
-        $total = $total+$cartItem->price;
+        $tran_id = uniqid();
+        $delivery_status = "Pending";
+        $payment_status = "Pending";
+
+        $Profile = CustomerProfile::where('user_id', '=', $user_id)->first();
+        $cus_details = "Name: $Profile->cus_name, Address: $Profile->cus_add, City: $Profile->cus_city, Phone: $Profile->cus_phone";
+        $ship_details = "Name: $Profile->ship_name, Address: $Profile->ship_add, City: $Profile->ship_city";
+
+        // Payable Calculator
+        $total = 0;
+        $cartList = ProductCart::where('user_id', '=', $user_id)->get();
+        forEach($cartList as $cartItem){
+            $total = $total+$cartItem->price;
+        }
+
+        $vat = ($total*3)/100;
+        $payable = $total+$vat;
+
+        $invoice = Invoice::create([
+            'total' => $total,
+            'vat'=> $vat,
+            'payable' => $payable,
+            'cus_details' =>$cus_details,
+            'ship_details' =>$ship_details,
+            'tran_id' =>$tran_id,
+            'delivery_status' => $delivery_status,
+            'payment_status' => $payment_status,
+            'user_id' => $user_id
+        ]);
+
+        
+
 
     }
-
-    $vat = ($total*3)/100;
-    $payable = $total+$vat;
-    
-
-   }
  
 }
