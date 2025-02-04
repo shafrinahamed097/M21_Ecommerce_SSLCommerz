8c2a7ce416d884b0b27acdb13fddc07a16de5458
shafrinahamed97 <shafrinahamedjob@gmail.com>
2025-02-04 21:20:42 +0600

    Declare hero slider carousel async function

--- resources/views/component/HeroSlider.blade.php
+++ resources/views/component/HeroSlider.blade.php
@@ -7,7 +7,9 @@
         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-bs-slide="next"><i class="ion-chevron-right"></i></a>
     </div>
 </div>
-<script>
+
+
+ <script>
     async function Hero() {
         let res = await axios.get("/ListProductSlider");
         $("#carouselSection").empty();
@@ -18,7 +20,7 @@
             }
             let SliderItem=`<div class="carousel-item  background_bg ${activeClass}" style="background-image: url('${item['image']}')">
                 <div class="banner_slide_content">
-                    <div class="container"><!-- STRART CONTAINER -->
+                    <div class="container">
                         <div class="row">
                             <div class="col-lg-7 col-9">
                                 <div class="banner_content overflow-hidden">
@@ -35,5 +37,43 @@
         })
     }
 </script>
+
+<!-- <script>
+    async function Hero(){
+        let res = await axios.get("/ListProductSlider");
+        $("#carouselSection").empty();
+        res.data['data'].forEach((item,i)=>{
+            let activeClass = ""
+            if(i==0){
+                activeClass = "active"
+            }
+            let sliderItem = `
+             <div class = "carousel-item background_bg ${activeClass}" style = "background-image: url("$item['image]") >
+               <div class = "banner_slide_content" >
+                 <div class = "container">
+                   <div class = "row">
+                    <div class = "col-lg-7 col-9">
+                    <div class = "banner_content overflow-hidden">
+                      <h5 class = "mb-3 staggered-animation font-weight-light" data-animation = "slideInLeft" data-animation-delay = "0.5s" >${item['price']}</h5>
+                      <h2 class = "staggered-animation" data-animation = "SideInLeft" data-animation-delay = "1s" >${item['title']}</h2>
+                      <a class = "btn btn-fill-out rounded-0 staggered-animation text-uppercase" href = "#" data-animation = "slideInLeft data-animation-delay="1.5s" >Shop Now</a>
+                    </div>
+                    </div>
+                   </div>
+                 
+                 </div>
+               </div
+             </div>
+            `
+
+            $("#carouselSection").append('sliderItem')
+        })
+
+    }
+</script> -->
+
+
+
+
 
 
